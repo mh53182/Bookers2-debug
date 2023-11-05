05bab3d01a199daf378af0865f8579afb61f9417
mh53182 <marie.i.077145@gmail.com>
2023-11-05 06:54:47 +0000

    [Add]検索機能

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,8 @@ Rails.application.routes.draw do
     end
   end
 
+  get "search", to: "searches#search"
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
