6e7250fe433803fd5b44bead26aea7fd803626d6
mh53182 <marie.i.077145@gmail.com>
2023-11-06 08:06:24 +0000

    [Update]検索機能

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,14 +4,17 @@ class FavoritesController < ApplicationController
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.new(book_id: book.id)
     favorite.save
-    redirect_to request.referer
+    # redirect_to request.referer
+    # 非同期通信化のためリダイレクトの記述を削除
+    # "render"や"redirect_to"の記述がない場合、自動で.js.erbを探しに行く。
+    # ＝リダイレクト先が無いからJavaScriptリクエストになる
   end
   
   def destroy
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.find_by(book_id: book.id)
     favorite.destroy
-    redirect_to request.referer
+    # redirect_to request.referer
   end
   
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -2,24 +2,25 @@ class Book < ApplicationRecord
   belongs_to :user
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
-  
+
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
-  
+
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
   end
-  
+
   def self.search_for(content, method)
     if method == "perfect"
-      Book.where("title: content")
+      Book.where('title = ? OR body = ?', content, content)
     elsif method == "forward"
-      Book.where("title LIKE ?", content + "%")
+      Book.where('title LIKE ? OR body LIKE ?', content + '%',content + '%')
     elsif method == "backward"
-      Book.where("title LIKE ?", "%" + content)
+      Book.where('title LIKE ? OR body LIKE ?', '%' + content,'%' + content)
     else
-      Book.where("title LIKE ?", "%" + content + "%")
+      Book.where('title LIKE ? OR body LIKE ?', '%' + content + '%','%' + content + '%')
     end
   end
-  
+  # 複数カラムを検索対象としてORで繋ぐ場合、検索フォームから送られてくるデータの受け口も検索対象と同じだけ必要。
+
 end
