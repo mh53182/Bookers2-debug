5ccf9680fbf372a9809049ec81045675b11cec6d
mh53182 <marie.i.077145@gmail.com>
2023-11-07 03:50:29 +0000

    [Update]コメント機能の非同期通信化

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,22 +1,21 @@
 class BookCommentsController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    if comment.save
-      redirect_to book_path(book.id)
-    else
-      render 'book_path(book.id)'
-    end
+    @book = Book.find(params[:book_id])
+    @comment = current_user.book_comments.new(book_comment_params)
+    @comment.book_id = @book.id
+    @comment.save
+    # redirect_to book_path(book.id)
+    # 非同期通信化のためリダイレクト先を削除
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    comment = book.book_comments.find(params[:id])
-    comment.destroy
-    # BookComment.find(params[:id]).destroy
-    redirect_to book_path(book)
+    @book = Book.find(params[:book_id])
+    @comment = @book.book_comments.find(params[:id])
+    @comment.destroy
+    # BookComment.find(params[:id]).destroy　←なんだっけこれ
+    # redirect_to book_path(book)
+    # 非同期通信化のためリダイレクト先を削除
   end
 
   private
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,20 +1,22 @@
 class FavoritesController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.new(book_id: @book.id)
     favorite.save
     # redirect_to request.referer
     # 非同期通信化のためリダイレクトの記述を削除
     # "render"や"redirect_to"の記述がない場合、自動で.js.erbを探しに行く
     # ＝リダイレクト先が無いからJavaScriptリクエストになる
+    # インスタンス変数にする！　たぶん。
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: @book.id)
     favorite.destroy
     # redirect_to request.referer
+    # 同上
   end
 
 end
